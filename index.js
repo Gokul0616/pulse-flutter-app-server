ac3cf01d8b3be8eef39c5f3ec2ccaa67fd705838
Gokul R <mistygokuloff@gmail.com>
2024-11-27 15:09:50 +0530

    profileimageupdatecompleted

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
-// import { sendEmail } from "./components/mail services/mailService.js";
+import { sendEmail } from "./components/mail services/mailService.js";
 
 // Resolve the current directory for ES module
 const __filename = fileURLToPath(import.meta.url);
@@ -27,26 +27,26 @@ app.use(cors());
 app.use(bodyParser.json());
 const SALT_ROUNDS = 10;
 // Utility function to send email by triggering the Python script
-const sendEmail = (recipient, subject, body) => {
-  return new Promise((resolve, reject) => {
-    // Construct the command to run the Python script
-    const command = `cd components/mail services && python3 send_email.py ${recipient} "${subject}" "${body}"`;
+// const sendEmail = (recipient, subject, body) => {
+//   return new Promise((resolve, reject) => {
+//     // Construct the command to run the Python script
+//     const command = `cd components/mail services && python3 send_email.py ${recipient} "${subject}" "${body}"`;
 
-    // Execute the Python script
-    exec(command, (error, stdout, stderr) => {
-      if (error) {
-        console.error(`exec error: ${error}`);
-        return reject(error);
-      }
-      if (stderr) {
-        console.error(`stderr: ${stderr}`);
-        return reject(stderr);
-      }
-      console.log(`stdout: ${stdout}`);
-      resolve(stdout);
-    });
-  });
-};
+//     // Execute the Python script
+//     exec(command, (error, stdout, stderr) => {
+//       if (error) {
+//         console.error(`exec error: ${error}`);
+//         return reject(error);
+//       }
+//       if (stderr) {
+//         console.error(`stderr: ${stderr}`);
+//         return reject(stderr);
+//       }
+//       console.log(`stdout: ${stdout}`);
+//       resolve(stdout);
+//     });
+//   });
+// };
 
 // Example route to send OTP via email
 app.post("/api/PhoneOrEmailValidate", async (req, res) => {
